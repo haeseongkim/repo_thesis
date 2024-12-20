e731fef6c4c9d0cadf325682afdc9fb149c4877b
haeseongkim <70826732+haeseongkim@users.noreply.github.com>
2024-12-20 08:47:04 -0500

    Delete Ensemble_Kalman_methods directory

--- Ensemble_Kalman_methods/k_T_heater.m
+++
@@ -1,7 +0,0 @@
-% output: thermal conductivity [W/m/K] heater material
-% input: temperature [K]
-function [k] = k_T_heater(T)
-    
-    k = 0.014748*(T-273.15) + 11.1839;
-    
-end
--- Ensemble_Kalman_methods/k_T_zro2.m
+++
@@ -1,7 +0,0 @@
-% output: thermal conductivity [W/m/K] zirconia
-% input: temperature [K]
-function [k] = k_T_zro2(T)
-    
-    k = 4.8427*10^-6*(T-273.15).^2 - 0.0098*(T-273.15) + 7.5149;
-    
-end
